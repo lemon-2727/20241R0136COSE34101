0c5be6cd2fa313b70cdf038ea4d3e160c41eef76
lemon-2727 <165107666+lemon-2727@users.noreply.github.com>
2024-06-05 10:14:08 +0900

    Update io_event.c

--- cpu scheduler/io_event.c
+++ cpu scheduler/io_event.c
@@ -33,7 +33,8 @@ int IOBurst(Process *runq, Process *waitq, int *size1, int *size2, int p_count, 
     //I/O burst cannot happen when there is another process in the waiting queue
     //there also must be a process in the running queue
     //I/O needs to happen at least 1 seconds before total time 
-    //for debugging printf("%d %d %d %d %d %d", num, *io_count, p_count, *size1, *size2, remaining_t);
+    //for debugging 
+    //printf("%d %d %d %d %d %d", num, *io_count, p_count, *size1, *size2, remaining_t);
     int var = 0;
     if((!(num ) && *io_count <= p_count/2) && (*size1 > 0 && *size2 == 0) && remaining_t > 1){
         printf("I/O interrupt! process id: %d\n", runq[0].id);
@@ -44,7 +45,9 @@ int IOBurst(Process *runq, Process *waitq, int *size1, int *size2, int p_count, 
         //!burst time is smaller than the total remaining time
         while(runq[0].burst_t > remaining_t){
             runq[0].burst_t = (rand() % (10 - 1 + 1)) + 1;
+            
         }
+        runq[0].ioburst_t = runq[0].burst_t;
 
         //move process to waiting queues
         addProcess(waitq, runq, size2, 0);
